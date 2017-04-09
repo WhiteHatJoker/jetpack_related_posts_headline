7181587ee65da2d96fd9dbef87381d2163629400
Roshan <rkhudoyberdiev@gmail.com>
2017-04-09 00:47:18 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,3 +1,4 @@
+//Change the related posts headline
 function jetpackme_related_posts_headline( $headline ) {
 $headline = sprintf(
             '<h3 class="jp-relatedposts-headline"><em>%s</em></h3>',
@@ -6,3 +7,6 @@ $headline = sprintf(
 return $headline;
 }
 add_filter( 'jetpack_relatedposts_filter_headline', 'jetpackme_related_posts_headline' );
+
+//Do not display the related post context
+add_filter( 'jetpack_relatedposts_filter_post_context', '__return_empty_string' );
